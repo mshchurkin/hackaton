c570e8f5f4b22327182ed044da689b982f1422fc
Анастасия <@ANASTASIYA>
2016-11-01 12:22:35 +0500

    бла бла бла, заебало все

--- hackaton/App_Start/RouteConfig.cs
+++ hackaton/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace hackaton
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Main", action = "Main", id = UrlParameter.Optional }
+                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
